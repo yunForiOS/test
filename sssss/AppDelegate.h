914128588548a14732d4d6d26d064dcdfe46f08d
yun <yun@yundemac.local>
2016-07-01 08:54:43 +0800

    Initial Commit

---
+++ sssss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sssss
+//
+//  Created by yun on 16/7/1.
+//  Copyright © 2016年 yun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sssss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sssss
+//
+//  Created by yun on 16/7/1.
+//  Copyright © 2016年 yun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
